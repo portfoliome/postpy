b5cff009f92a8b3ee1b67853406b2ec24a53cc0a
pmart123 <philip.martin2007@gmail.com>
2016-12-23 16:33:00 -0500

    Specification for basic db admin functionality. * Reflecting table/column basic attributes. * Reflecting user tables. * primary keys

---
+++ pgdabble/admin.py
@@ -0,0 +1,73 @@
+"""
+Database administration queries
+"""
+
+from pgdabble.base import Table, Column, PrimaryKey
+from pgdabble.ddl import compile_qualified_name
+from pgdabble.sql import select_dict
+
+
+def get_user_tables(conn):
+    """Retrieve all user tables."""
+
+    query_string = "select schemaname, relname from pg_stat_user_tables;"
+    with conn.cursor() as cursor:
+        cursor.execute(query_string)
+        tables = cursor.fetchall()
+
+    return tables
+
+
+def get_primary_keys(conn, table: str, schema='public'):
+    """Returns primary key columns for a specific table."""
+
+    query = """\
+SELECT
+  c.constraint_name as pkey_constraint_name,
+  c.column_name as column_name
+FROM
+  information_schema.key_column_usage AS c
+LEFT JOIN information_schema.table_constraints AS t
+  ON t.constraint_name=c.constraint_name
+WHERE t.constraint_type = 'PRIMARY KEY'
+  AND c.table_schema=%s
+  AND c.table_name=%s;"""
+
+    for record in select_dict(conn, query, params=(schema, table)):
+        yield record['column_name']
+
+
+def get_column_metadata(conn, table: str, schema='public'):
+    """Returns column data following db.Column parameter specification."""
+    query = """\
+SELECT
+  attname as name,
+  format_type(atttypid, atttypmod) AS data_type,
+  NOT attnotnull AS nullable
+FROM pg_catalog.pg_attribute
+WHERE attrelid=%s::regclass
+  AND attnum > 0 AND NOT attisdropped
+ORDER BY attnum;"""
+
+    qualified_name = compile_qualified_name(table, schema=schema)
+
+    for record in select_dict(conn, query, params=(qualified_name,)):
+        yield record
+
+
+def reflect_table(conn, table_name, schema='public'):
+    """Reflect basic table attributes."""
+
+    column_meta = list(get_column_metadata(conn, table_name, schema=schema))
+    primary_key_columns = list(get_primary_keys(conn, table_name, schema=schema))
+
+    columns = [Column(**column_data) for column_data in column_meta]
+    primary_key = PrimaryKey(primary_key_columns)
+
+    return Table(table_name, columns, primary_key, schema=schema)
+
+
+def pg_stats_extension_statement():
+    """postgres stats extensions statement."""
+
+    return 'CREATE EXTENSION IF NOT EXISTS pg_stat_statements;'
---
+++ tests/test_admin.py
@@ -0,0 +1,71 @@
+import unittest
+
+from pgdabble.admin import (get_user_tables,
+                            get_column_metadata,
+                            reflect_table, get_primary_keys)
+from pgdabble.base import Table, Column, PrimaryKey
+from .common import PostgreSQLFixture
+
+
+class TestTableStats(PostgreSQLFixture, unittest.TestCase):
+
+    @classmethod
+    def _prep(cls):
+        cls.conn.autocommit = True
+        cls.schema = 'stats_test'
+        cls.table = 'admin_table_tests'
+        create_table_statement = """\
+            CREATE TABLE {schema}.{table} (
+              mycol CHAR(2),
+              mycol2 CHAR(3) NULL,
+              PRIMARY KEY (mycol));""".format(schema=cls.schema,
+                                              table=cls.table)
+
+        with cls.conn.cursor() as cursor:
+            cursor.execute('CREATE SCHEMA {};'.format(cls.schema))
+            cursor.execute(create_table_statement)
+
+    def test_get_user_tables(self):
+
+        expected = (self.schema, self.table)
+        result = get_user_tables(self.conn)
+
+        self.assertIn(expected, result)
+
+    def test_get_column_meta_data(self):
+        expected = [
+            {'name': 'mycol',
+             'data_type': 'character(2)',
+             'nullable': False},
+            {'name': 'mycol2',
+             'data_type': 'character(3)',
+             'nullable': True}
+        ]
+        result = list(
+            get_column_metadata(self.conn, self.table, schema=self.schema)
+        )
+
+        self.assertEqual(expected, result)
+
+    def test_get_primary_keys(self):
+        expected = ['mycol']
+        result = list(get_primary_keys(self.conn, self.table, self.schema))
+
+        self.assertEqual(expected, result)
+
+    def test_reflect_table(self):
+        columns = [Column('mycol', data_type='character(2)', nullable=False),
+                   Column('mycol2', data_type='character(3)', nullable=True)]
+        primary_key = PrimaryKey(['mycol'])
+
+        expected = Table(self.table, columns, primary_key, schema=self.schema)
+        result = reflect_table(self.conn, self.table, self.schema)
+
+        self.assertEqual(expected, result)
+
+    @classmethod
+    def _clean(cls):
+        statement = 'DROP SCHEMA IF EXISTS {} CASCADE;'.format(cls.schema)
+
+        with cls.conn.cursor() as cursor:
+            cursor.execute(statement)
